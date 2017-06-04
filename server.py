025316f714372bc4ba0fd079981eb4de13f231c7
Rachel P.Y. Liao <u3512485@connect.hku.hk>
2017-06-04 11:18:12 +0800

    add allow headers

--- server.py
+++ server.py
@@ -70,7 +70,7 @@ class HTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
 		self.send_header('Access-Control-Allow-Origin', self.headers['Origin'])
 		self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
-		self.send_header('Access-Control-Request-Headers', 'Content-Type')
+		self.send_header('Access-Control-Allow-Headers', 'Content-Type')
 		self.end_headers()
  
 def run():
